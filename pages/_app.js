f37d791476838c7d86ee1962712bb7fcad125765
Mateusz Jakowlew <mtjako@Mateuszs-Mac-mini.local>
2021-04-28 14:34:02 +0200

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
-import '../styles/style.css'
+import "../styles/style.css";
 import { Footer } from "../components/Footer";
 import { Navigation } from "../components/Navigation/Navigation";
 import { GlobalStyles } from "../styles/global";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "../styles/theme";
 import React, { useState, useEffect } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const [theme, setTheme] = useState("light");
@@ -28,12 +29,28 @@ function MyApp({ Component, pageProps }) {
     }
   };
   return (
-    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-      <GlobalStyles />
-      <Navigation theme={theme} toggleTheme={toggleTheme} />
-      <Component {...pageProps} />
-      <Footer />
-    </ThemeProvider>
+    <>
+      <Head>
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-QNYKK5K2KZ"
+        ></script>
+        <script dangerouslySetInnerHTML={{__html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.GOOGLE_ANALYTICS}');
+        `}}/>
+      </Head>
+      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+        <GlobalStyles />
+        <Navigation theme={theme} toggleTheme={toggleTheme} />
+        <Component {...pageProps} />
+        <Footer />
+      </ThemeProvider>
+    </>
   );
 }
 
