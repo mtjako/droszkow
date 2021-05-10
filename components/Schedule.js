8f8b1100bbbe39991cdd8e5eae5dbd06a19e6049
Mateusz Jakowlew <mtjako@Mateuszs-Mac-mini.local>
2021-05-10 12:05:42 +0200

    repair schedule

--- components/Schedule.js
+++ components/Schedule.js
@@ -10,35 +10,44 @@ const Wrapper = styled.div`
 const DayWrap = styled.div`
   margin-bottom: 20px;
   h2{
-    padding: 4px 0;
+    padding: 12px 0;
   }
-  table{
-    width: 100%;
-    tr{
-      td{
-        padding: 4px 0;
-        &:nth-child(1){
-          width: 60px;
-          @media(min-width: 500px){
-            width: 120px;
-          }
-          text-align: left;
-        }
-        &:nth-child(2){
-          width: calc(100% - 260px);
-          text-align: left;
-          padding: 4px;
-        }
-        &:nth-child(3){
-          width: 75px;
-          @media(min-width: 500px){
-            width: 140px;
-          }
-          text-align: right;
+`;
 
-        }
+const Item = styled.div`
+  display: grid;
+  margin-bottom: 8px;
+  grid-column-gap: 16px;
+  grid-template-columns: 50px 1fr;
+  .time{
+    grid-column: 1/2;
+    grid-row: span 2;
+    text-align: center;
+    p{
+      font-weight: bold;
+      color: ${({ theme }) => theme.grey500};
+    }
+    p:nth-child(1){
+      position: relative;
+      padding-bottom: 4px;
+      &::after{
+        content: '';
+        position: absolute;
+        bottom: -1px;
+        left: 15px;
+        width: 20px;
+        height: 2px;
+        background-color: ${({ theme }) => theme.grey300};
       }
     }
+  }
+  .title{
+    grid-column: 2/3;
+    font-weight: bold;
+  }
+  .room{
+    grid-column: 2/3;
+    text-transform: uppercase;
   }
 `;
 
@@ -46,17 +55,20 @@ const Day = ({ name,data }) => {
   return (
     <DayWrap>
       <h2 className="">{name}</h2>
-      <table cellSpacing="0" cellpadding="0">
         {data.tbody.map((item, i) => (
-          <tr key={i}>
-            {item.body.map((item, i) => (
-              <td className="" key={i}>
-                <p>{item.value}</p>
-              </td>
-            ))}
-          </tr>
+          <Item key={i}>
+            {item.body.map((item, i) => {
+              if(i==0){
+                const text = item.value.split('-');
+                return(<div className="time" key={i}><p>{text[0]}</p><p>{text[1]}</p></div>)
+              }
+              if(i==1){
+                return(<p className="title" key={i}>{item.value}</p>)
+              }
+              return(<p className="room meta" key={i}>{item.value}</p>)
+            })}
+          </Item>
         ))}
-      </table>
     </DayWrap>
   );
 };
